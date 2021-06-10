0ca5c2a1c31a275d8c6eabc59ba976fb6333ac93
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 10:45:23 -0600

    Initial Commit

---
+++ DownloadJSONWithCompletionClosuresDemo/DownloadJSONWithCompletionClosuresDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadJSONWithCompletionClosuresDemoApp.swift
+//  DownloadJSONWithCompletionClosuresDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DownloadJSONWithCompletionClosuresDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
